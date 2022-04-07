c6a16c38093c032a6870742c777fe71dedf9a1e3
Chris Moss <hcmoss70@gmail.com>
2022-04-07 09:50:24 +0200

    new commit

---
+++ 7kyu/twoOldestAges.js
@@ -0,0 +1,15 @@
+// The two oldest ages function/method needs to be completed. 
+// It should take an array of numbers as its argument and return the
+//  two highest numbers within the array. The returned value should 
+//  be an array in the format [second oldest age, oldest age].
+
+// The order of the numbers passed in could be any order. 
+// The array will always include at least 2 items. If there
+//  are two or more oldest age, then return both of them in array format.
+
+function twoOldestAges(ages){
+    let oldest = ages.sort((a,b)=>b-a).shift()
+    let secOldest = ages.shift()
+    return [secOldest, oldest]
+}
+
