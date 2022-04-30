db36e27ef07cdb4502cde032e94ef7b588ff2ff5
Chris Moss <hcmoss70@gmail.com>
2022-04-30 15:19:07 +0200

    new commit

--- 7kyu/tempCodeRunnerFile.js
+++ 7kyu/tempCodeRunnerFile.js
@@ -1,7 +1,7 @@
   if(s.length %2==0){
-//     console.log( s.substring(s.length/2 -1 ,s.length/2+1 ) )
-//     }
-// else if(s.length%2!=0){
-//     console.log( s.substring(s.length/2 ,s.length/2+1) )
+    console.log( s.substring(s.length/2 -1 ,s.length/2+1 ) )
+    }
+else if(s.length%2!=0){
+    console.log( s.substring(s.length/2 ,s.length/2+1) )
    
-//   }
+  }
