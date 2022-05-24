2856e405580d55ffbfaf297396a86e282473d294
Chris Moss <hcmoss70@gmail.com>
2022-05-24 09:42:23 +0200

    new commit

---
+++ 8kyu/fakeBinary.js
@@ -0,0 +1,12 @@
+// Given a string of digits, you should replace any digit below 5 with '0'
+//  and any digit 5 and above with '1'. Return the resulting string.
+
+// Note: input will never be an empty string
+
+const fakeBin = x=> x.split('').map(x=> Number(x<5) ? 0 : 1).join('').toString()
+
+  
+    
+
+
+fakeBin('45385593107843568')
