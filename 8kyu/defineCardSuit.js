74573276df5baefc50c2dcb1e32a8616239be8f2
Chris Moss <hcmoss70@gmail.com>
2022-06-10 10:37:22 +0200

    new commit

---
+++ 8kyu/defineCardSuit.js
@@ -0,0 +1,18 @@
+// You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+// Our deck (is preloaded):
+
+// ('3♣') -> return 'clubs'
+// ('3♦') -> return 'diamonds'
+// ('3♥') -> return 'hearts'
+// ('3♠') -> return 'spades'
+
+function defineSuit(card) {
+    let one = card.slice(-1)
+    switch(one){     
+      case('♣'): return `clubs`; break;
+      case('♦'): return `diamonds`; break;
+      case('♥'): return `hearts`; break;
+      case('♠'): return `spades`; break;
+    }
+  }
